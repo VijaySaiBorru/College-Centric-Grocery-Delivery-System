c5f4386a3659c75d07bc5872cf673a1df7663bf1
VijaySaiBorru <22cs01076@iitbbs.ac.in>
2025-03-16 18:42:04 +0530

    updated Pictures

--- frontend/src/pages/home/Teams.jsx
+++ frontend/src/pages/home/Teams.jsx
@@ -1,27 +1,27 @@
-import chennaiSuperKings from "../../assets/chennai-super-kings.jpg";
-import mumbaiIndians from "../../assets/mumbai-indians.jpg";
-import kolkataKnightRiders from "../../assets/kolkata-knight-riders.jpg";
-import royalChallengersBangalore from "../../assets/royal-challengers-bangalore.jpg";
-import gujaratTitans from "../../assets/gujarat-titans.jpg";
-import lucknowSuperGiants from "../../assets/lucknow-super-giants.jpg";
-import sunrisersHyderabad from "../../assets/sunrisers-hyderabad.jpg";
-import rajasthanRoyals from "../../assets/rajasthan-royals.jpg";
-import punjabKings from "../../assets/punjab-kings.jpg";
-import delhiCapitals from "../../assets/delhi-capitals.jpg";
+import clothing from "../../assets/clothing.png";
+import bakery from "../../assets/bakery.png";
+import chips from "../../assets/chips.png";
+import electronics from "../../assets/electronics.png";
+import fruits from "../../assets/fruits.png";
+import groceries from "../../assets/groceries.png";
+import soft_drinks from "../../assets/soft_drinks.png";
+import stationary from "../../assets/stationary.png";
+import vegetables from "../../assets/vegetables.png";
+import medicine from "../../assets/medicine.png";
 import { Link } from 'react-router-dom';
 
 const Teams = () => {
   const teams = [
-    { name: 'Chennai Super Kings', path: 'chennai-super-kings', image: chennaiSuperKings },
-    { name: 'Mumbai Indians', path: 'mumbai-indians', image: mumbaiIndians },
-    { name: 'Kolkata Knight Riders', path: 'kolkata-knight-riders', image: kolkataKnightRiders },
-    { name: 'Royal Challengers Bangalore', path: 'royal-challengers-bangalore', image: royalChallengersBangalore },
-    { name: 'Gujarat Titans', path: 'gujarat-titans', image: gujaratTitans },
-    { name: 'Lucknow Super Giants', path: 'lucknow-super-giants', image: lucknowSuperGiants },
-    { name: 'Sunrisers Hyderabad', path: 'sunrisers-hyderabad', image: sunrisersHyderabad },
-    { name: 'Rajasthan Royals', path: 'rajasthan-royals', image: rajasthanRoyals },
-    { name: 'Punjab Kings', path: 'punjab-kings', image: punjabKings },
-    { name: 'Delhi Capitals', path: 'delhi-capitals', image: delhiCapitals },
+    { name: 'Clothing and Accessories', path: 'clothing', image: clothing },
+    { name: 'Bakery Items', path: 'bakery', image: bakery },
+    { name: 'Chips and Fried Items', path: 'chips', image: chips },
+    { name: 'Electronic Gadgets', path: 'electronics', image: electronics },
+    { name: 'Fruits', path: 'fruits', image: fruits },
+    { name: 'Groceries', path: 'groceries', image: groceries },
+    { name: 'Soft Drinks and Juices', path: 'soft_drinks', image: soft_drinks },
+    { name: 'Stationary', path: 'stationary', image: stationary },
+    { name: 'Vegeetables', path: 'vegetables', image: vegetables },
+    { name: 'Medicine', path: 'medicine', image: medicine },
   ];
 
   return (
